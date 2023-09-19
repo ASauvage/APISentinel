8d8f72398822fd8eefa1eba6f12406662495dd68
Alexandre <52382747+ASauvage@users.noreply.github.com>
2023-09-19 17:13:57 +0200

    Update global_tester.py

--- models/global_tester.py
+++ models/global_tester.py
@@ -1,5 +1,7 @@
+import json
 import os
-
+import json
+from datetime import datetime
 from utils.commons import generate_session_id
 from models.tester import apitester
 from models.service import Service
@@ -13,11 +15,13 @@ class GlobalTester:
 
         self.tests = list()
 
+        print("Your session ID: " + self.session_id)
         for file in os.listdir('{}/data/mapping/{}'.format(os.getcwd(), self.service.path)):
-            self.test_executer(filename=file, api=file[:-5])
+            self.test_executer(filename=file, api=file[:-5], headers=self.service.headers)
 
-        # FixMe elk request
-
+        with open('{}/data/saved/{}.json'.format(os.getcwd(), self.session_id), 'w') as file:
+            json.dump(self.tests, file, ensure_ascii=False, indent=4)
+        print("\nYour session ID: " + self.session_id)
 
     def test_executer(self, **specifications):
         errors = apitester(self.env, self.service, **specifications)
@@ -31,4 +35,6 @@ class GlobalTester:
             "errors": [error.__str__() for error in errors],
 
             "request": self.service.url(self.env, specifications['api']),
+            "datetime": datetime.now().strftime("%d %b %Y - %H:M:S")
         })
+        print("F" if errors else ".", end='')
