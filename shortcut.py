965f5efaaa98ff05f2f20bd528a7885489678935
Alexandre SAUVAGE POTEL <alexandre.sauvagepotel@ratpsmartsystems.com>
2024-10-28 10:54:26 +0100

    [feature] headers param when calling GlobalTester

--- shortcut.py
+++ shortcut.py
@@ -2,5 +2,5 @@ from models.global_tester import GlobalTester
 
 
 class PokeAPIGlobalTester(GlobalTester):
-    def __init__(self, env):
-        super().__init__(env, "pokeapi")
+    def __init__(self, env: str, headers: dict = None):
+        super().__init__(env, "pokeapi", headers)
