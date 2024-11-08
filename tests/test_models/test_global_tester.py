f940ad1d4a9cc0c3c3cd82088ad40055de458df6
Alexandre <a.sauvagepotel@gmail.com>
2024-11-08 14:19:58 +0100

    [doc] reminder because i'm busy

--- tests/test_models/test_global_tester.py
+++ tests/test_models/test_global_tester.py
@@ -4,7 +4,7 @@ from unittest.mock import patch
 
 class TestGlobalTester(unittest.TestCase):
     def test_init(self):
-        pass
+        pass  # ToDo
 
     def test_test_executer(self):
-        pass
+        pass  # ToDo
