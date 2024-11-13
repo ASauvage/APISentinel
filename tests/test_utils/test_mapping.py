5baf4141745c651c543da358ed5a34a1d1054c5f
Alexandre <a.sauvagepotel@gmail.com>
2024-11-13 13:28:56 +0100

    [UT] test: models.errors, utils.mapping

--- tests/test_utils/test_mapping.py
+++ tests/test_utils/test_mapping.py
@@ -35,7 +35,7 @@ class TestMapping(unittest.TestCase):
         assert len(errors) == 1
         assert isinstance(errors[0], MissingFieldError)
         assert errors[0].as_dict() == dict(explicit_content="MissingFieldError", field=[0, 'metadata', 'request_id'])
-        assert errors[0].__str__() == "MissingFieldError: field '[0, 'metadata', 'request_id']' is missing"
+        assert errors[0].__str__() == "MissingFieldError: field '0.metadata.request_id' is missing"
 
     def test_is_mapping_ok_wrong_type_error(self):
         alt_data = deepcopy(DATA)
@@ -46,7 +46,7 @@ class TestMapping(unittest.TestCase):
         assert isinstance(errors[0], WrongTypeError)
         assert errors[0].as_dict() == dict(explicit_content="WrongTypeError", field=[0, 'metadata', 'request_id'],
                                            expected=[int, float], received=str)
-        assert errors[0].__str__() == "WrongTypeError: '[0, 'metadata', 'request_id']' should be a(n) [<class 'int'>, <class 'float'>] not <class 'str'>"
+        assert errors[0].__str__() == "WrongTypeError: '0.metadata.request_id' should be a(n) ['int', 'float'] not 'str'"
 
     def test_is_mapping_ok_wrong_value_error(self):
         alt_data = deepcopy(DATA)
@@ -57,7 +57,7 @@ class TestMapping(unittest.TestCase):
         assert isinstance(errors[0], WrongValueError)
         assert errors[0].as_dict() == dict(explicit_content="WrongValueError", field=[0, 'enums'],
                                            expected=['on', 'off', 'asauvage'], received='sauvagea')
-        assert errors[0].__str__() == "WrongValueError: '[0, 'enums']' should be in ['on', 'off', 'asauvage'] but got 'sauvagea' instead"
+        assert errors[0].__str__() == "WrongValueError: '0.enums' should be in ['on', 'off', 'asauvage'] but got 'sauvagea' instead"
 
     def test_is_mapping_ok_wrong_format_error(self):
         alt_data = deepcopy(DATA)
@@ -68,7 +68,7 @@ class TestMapping(unittest.TestCase):
         assert isinstance(errors[0], WrongFormatError)
         assert errors[0].as_dict() == dict(explicit_content="WrongFormatError", field=[0, 'metadata', 'request_id'],
                                            expected='int', received=float)
-        assert errors[0].__str__() == "WrongFormatError: '[0, 'metadata', 'request_id']' should be a(n) int but got '<class 'float'>' instead"
+        assert errors[0].__str__() == "WrongFormatError: '0.metadata.request_id' should be a(n) 'int' but got 'float' instead"
 
     def test_is_mapping_ok_wrong_datetime_format_error(self):
         alt_data = deepcopy(DATA)
@@ -79,7 +79,7 @@ class TestMapping(unittest.TestCase):
         assert isinstance(errors[0], WrongDatetimeFormatError)
         assert errors[0].as_dict() == dict(explicit_content="WrongFormatError", field=[0, 'metadata', 'datetime'],
                                            format='datetime', received='not a datetime')
-        assert errors[0].__str__() == "WrongFormatError: '[0, 'metadata', 'datetime']' not in datetime format, got 'not a datetime'"
+        assert errors[0].__str__() == "WrongFormatError: '0.metadata.datetime' does not follow the 'datetime' format, got 'not a datetime'"
 
     def test_is_mapping_ok_regex_error(self):
         alt_data = deepcopy(DATA)
@@ -90,7 +90,7 @@ class TestMapping(unittest.TestCase):
         assert isinstance(errors[0], RegexError)
         assert errors[0].as_dict() == dict(explicit_content="RegexError", field=[0, 'data', 1],
                                            pattern='^[a-z]*$', value='pl34s3')
-        assert errors[0].__str__() == "RegexError: '[0, 'data', 1]' should match pattern '^[a-z]*$' but got 'pl34s3'"
+        assert errors[0].__str__() == "RegexError: '0.data.1' should match pattern '^[a-z]*$' but got 'pl34s3'"
 
     def test_is_mapping_ok_minlen_error(self):
         alt_data = deepcopy(DATA)
@@ -101,7 +101,7 @@ class TestMapping(unittest.TestCase):
         assert isinstance(errors[0], MinLenghtError)
         assert errors[0].as_dict() == dict(explicit_content="MinLenghtError", field=[0, 'data'],
                                            expected=1, received=0)
-        assert errors[0].__str__() == "MinLenghtError: '[0, 'data']' got 0 values but 1 minimum required"
+        assert errors[0].__str__() == "MinLenghtError: '0.data' got 0 values but 1 minimum required"
 
     def test_is_mapping_ok_maxlen_error(self):
         alt_data = deepcopy(DATA)
@@ -112,4 +112,4 @@ class TestMapping(unittest.TestCase):
         assert isinstance(errors[0], MaxLenghtError)
         assert errors[0].as_dict() == dict(explicit_content="MaxLenghtError", field=[0, 'data'],
                                            expected=3, received=4)
-        assert errors[0].__str__() == "MaxLenghtError: '[0, 'data']' got 4 values but 3 maximum required"
+        assert errors[0].__str__() == "MaxLenghtError: '0.data' got 4 values but 3 maximum required"
