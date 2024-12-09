0ce3cba6a1e1299c5c09bc0724b2f96a4dcb6424
Alexandre <a.sauvagepotel@gmail.com>
2024-12-09 16:27:56 +0100

    [UT] test: models.global_tester

--- tests/test_models/test_global_tester.py
+++ tests/test_models/test_global_tester.py
@@ -6,13 +6,13 @@ from models.global_tester import GlobalTester
 class MockedService:
     path = 'test_path'
     options = {'request_delay': 100}
-    headers = {"key": "value"}
+    headers = {'key': 'value'}
 
     def __init__(self, name: str):
         self.name = name
 
     def url(self, env, api):
-        return "http://example.com"
+        return 'http://example.com'
 
 
 class TestGlobalTester(unittest.TestCase):
@@ -22,9 +22,16 @@ class TestGlobalTester(unittest.TestCase):
     @patch('yaml.load', return_value=dict(extended_paths=['/extra']))
     @patch('utils.mongodb.MongoCon.save_results')
     def test_test_executer_success(self, mockpatch_apitester, mockpatch_service, mockpatch_listdir, mockpatch_load, mockpatch_save_results):
-        tester = GlobalTester("production", "TestService")
+        tester = GlobalTester('production', 'TestService')
+        print(tester.tests[0])
         assert len(tester.tests) == 1
+        assert tester.tests[0]['title'] == 'Test on /test_api'
+        assert tester.tests[0]['env'] == 'production'
+        assert tester.tests[0]['service'] == 'TestService'
+        assert tester.tests[0]['request'] == 'http://example.com'
+        assert tester.tests[0]['headers'] == {'User-Agent': 'test-mapping', 'referer': 'test-mapping', 'key': 'value'}
         assert tester.tests[0]['status']
+        assert len(tester.tests[0]['errors']) == 0
         mockpatch_save_results.assert_called_once()
 
     @patch('models.global_tester.apitester', return_value=['fake_error_object'])
@@ -33,8 +40,13 @@ class TestGlobalTester(unittest.TestCase):
     @patch('yaml.load', return_value=dict(extended_paths=['/extra']))
     @patch('utils.mongodb.MongoCon.save_results')
     def test_test_executer_failure(self, mockpatch_apitester, mockpatch_service, mockpatch_listdir, mockpatch_load, mockpatch_save_results):
-        tester = GlobalTester("production", "TestService")
+        tester = GlobalTester('production', 'TestService')
         assert len(tester.tests) == 1
+        assert tester.tests[0]['title'] == 'Test on /test_api'
+        assert tester.tests[0]['env'] == 'production'
+        assert tester.tests[0]['service'] == 'TestService'
+        assert tester.tests[0]['request'] == 'http://example.com'
+        assert tester.tests[0]['headers'] == {'User-Agent': 'test-mapping', 'referer': 'test-mapping', 'key': 'value'}
         assert not tester.tests[0]['status']
         assert len(tester.tests[0]['errors']) == 1
         mockpatch_save_results.assert_called_once()
