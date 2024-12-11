ae9809816134e1f89ad7481d2c0386fc3bde991a
Alexandre <a.sauvagepotel@gmail.com>
2024-12-11 12:16:05 +0100

    [feature] mapping tester simulation

--- utils/mapping_tester.py
+++ utils/mapping_tester.py
@@ -1,84 +1,54 @@
+import os
 import json
 
-
-TYPE = [
-    'Object',
-    'Array',
-    'String',
-    'Number',
-    'Boolean',
-    'Null'
-]
+# What it should do
+# - build response from mapping
+# - return errors if there are
 
 
-def recursive_test(path: list, mapping: dict, errors: list) -> list:
-    for field_name, field_value in mapping.items():
-        value_path = path + [field_name] if field_name != "_tmpfield" else path
-        try:
-            value_type = field_value['_type']
+def integrity_test(mapping_path: str) -> list:
+    errors = list()
 
-            if value_type not in TYPE:
-                errors.append("UnknownType: type '{}' is not an existing type in field {}".format(value_type, value_path))
+    mapping = get_mapping(mapping_path)
 
-            if value_type == 'Object':
-                recursive_test(value_path, field_value['_properties'], errors)
-
-            elif value_type == 'Array':
-                for index in range(len(field_value['_values'])):
-                    recursive_test(value_path + [index], {'_tmpfield':field_value['_values'][index]}, errors)
-
-        except KeyError as e:
-            errors.append("RequiredFieldMissing: field {} is required".format(value_path + [e.args[0]]))
+    # todo check fields properties (warning for aditional)
 
     return errors
 
 
-def integrity_test(mapping_path: str) -> list:
-    with open(mapping_path, 'r') as file:
-        mapping_json = json.load(file)
-
-        return recursive_test(list(), mapping_json, list())
-
-
-def response_simulation(path: list, mapping: dict) -> dict:
-    response_tmp = dict()
-    for field_name, field_value in mapping.items():
-        value_path = path + [field_name] if field_name != "_tmpfield" else path
-        value_type = field_value['_type']
-
-        if value_type == 'Object':
-            response_tmp[field_name] = response_simulation(value_path, field_value['_properties'])
-
-        elif value_type == 'Array':
-            value = list()
-            for index in range(len(field_value['_values'])):
-                value.append(response_simulation(value_path, {'_tmpfield':field_value['_values'][index]})['_tmpfield'])
-
-            response_tmp[field_name] = value
-
-        else:
-            response_tmp[field_name] = value_type
-
-    return response_tmp
-
-
 def simulation_test(mapping_path: str) -> dict:
-    with open(mapping_path, 'r') as file:
-        mapping_json = json.load(file)
+    mapping = get_mapping(mapping_path)
 
-        return response_simulation(list(), mapping_json)
+    def simulate_field(path: list, mapping: dict) -> dict:
+        response_tmp = dict()
+        for field_name, field_value in mapping.items():
+            value_path = path + [field_name] if field_name != "_tmpfield" else path
+            value_type = field_value['_type']
+
+            if value_type == 'Object':
+                try:
+                    response_tmp[field_name] = simulate_field(value_path, field_value['_properties'])
+                except KeyError:
+                    response_tmp[field_name] = dict()
+            elif value_type == 'Array':
+                value = list()
+                try:
+                    for index in range(len(field_value['_values'])):
+                        value.append(simulate_field(value_path, {'_tmpfield': field_value['_values'][index]})['_tmpfield'])
+
+                    response_tmp[field_name] = value
+                except KeyError:
+                    response_tmp[field_name] = list()
+            else:
+                response_tmp[field_name] = f"{value_type}/{field_value['_format']}" if '_format' in field_value.keys() else value_type
+
+        return response_tmp
+
+    return simulate_field([], mapping)['__unamed__']
 
 
-if __name__ == "__main__":
-    mapping = "./data/mapping/pokeapi/pokemon.json"
-
-    errors = integrity_test(mapping)
-
-    if errors:
-        for error in errors:
-            print(error)
-    else:
-        print("This mapping file is correct")
-        with open("./output.json", 'w') as file:
-            json.dump(simulation_test(mapping), file, indent=4)
-
+def get_mapping(mapping_path: str) -> dict:
+    if not mapping_path.endswith('.json'):
+        mapping_path += '.json'
+    with open('{}/data/mapping/{}'.format(os.getcwd(), mapping_path), 'r') as file:
+        return {"__unamed__": json.load(file)}
